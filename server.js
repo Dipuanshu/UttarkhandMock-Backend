b30c12e5851441ba5c2ef917f9df6fa366e2ca13
Deepu <deepujoshi552@gmail.com>
2025-12-04 13:10:32 +0530

    Fix File Problem

--- server.js
+++ server.js
@@ -3,9 +3,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import connectDB from "./config/db.js";
 
-// import authRoutes from "./routes/authRoutes.js";
 import notesRoutes from "./routes/notesRoutes.js";
 import testRoutes from "./routes/testRoutes.js";
 
@@ -15,9 +16,14 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
 
-// app.use("/api/auth", authRoutes);
+// Fix for __dirname in ES modules (Render Compatible)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve uploads folder statically
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 app.use("/api/notes", notesRoutes);
 app.use("/api/tests", testRoutes);
 
